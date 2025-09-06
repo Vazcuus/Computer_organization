dd99997fe12eba9b50e07bd79eb6f1255f85f1b8
Vazcuus <levkonst12@gmail.com>
2025-09-06 11:15:26 +0700

    Added four input numbers and a flip.

--- Lab_1/Lab_source.asm
+++ Lab_1/Lab_source.asm
@@ -7,8 +7,51 @@ start:
 	
 	mov si, my_group  ; загрузка адреса строки с группой
     call print_string
+
+
+    call input_number
+    call reverse_number
+    call print_reversed
+
 	
     jmp $   
+
+input_number:
+    mov si, prompt_msg
+    call print_string
+    mov di, input_buffer
+    mov cx, 4
+.input_loop:
+    mov ah, 0x00
+    int 0x16
+    cmp al, '0'
+    jb .input_loop
+    cmp al, '9'
+    ja .input_loop
+    stosb
+    mov ah, 0x0e
+    int 0x10
+    loop .input_loop
+    ret
+
+; Процедура переворота числа
+reverse_number:
+    mov si, input_buffer
+    mov di, reversed_buffer + 3
+    mov cx, 4
+.reverse_loop:
+    lodsb
+    mov [di], al
+    dec di
+    loop .reverse_loop
+    ret
+; Процедура печати перевернутого числа
+print_reversed:
+    mov si, result_msg
+    call print_string
+    mov si, reversed_buffer
+    call print_string
+    ret
 
 print_string:	
     mov ah, 0x0e        ; функция teletype BIOS
@@ -25,6 +68,14 @@ my_lastname:
     db "Konstantinov ", 0 ; строка с нулевым терминатором
 my_group:
 	db "8B31 ", 0 ; строка с нулевым терминатором
+prompt_msg:
+    db 0x0D, 0x0A, "Enter 4-digit number: ", 0
+result_msg:
+    db 0x0D, 0x0A, "Reversed: ", 0
+input_buffer:
+    times 5 db 0
+reversed_buffer:
+    times 5 db 0
 
 times 510-($-$$) db 0   ; заполняем нулями до 510 байта
 dw 0xaa55               ; сигнатура загрузчика
